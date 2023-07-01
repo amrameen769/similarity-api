3a75918886130c5d7651ceb9eab336dc10b125c2
Al Ameen <amrameen769@gmail.com>
2023-07-02 00:57:40 +0530

    Added till DropdownMenu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,34 @@
-import { FC } from 'react'
+import { getServerSession } from "next-auth"
+import Link from "next/link";
+import { buttonVariants } from "@/ui/Button";
+import SignInButton from "@/components/SignInButton";
+import SignOutButton from "@/components/SignOutButton";
 
-interface NavBarProps {
-  
-}
+const NavBar = async () => {
+    const session = await getServerSession();
 
-const NavBar: FC<NavBarProps> = ({}) => {
-  return <div>NavBar</div>
+    return <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between w-full">
+        <div className="container max-w-7xl mx-auto w-full justify-between items-center flex">
+            <Link href="/" className={buttonVariants({ variant: 'link' })}>
+                Similarity API One.O
+            </Link>
+
+            <div className="md:hidden">
+                {/* <ThemeToggle /> */}
+            </div>
+
+            <div className="hidden: md:flex gap-4">
+                {/* <ThemeToggle /> */}
+                <Link href={'/documentation'} className={buttonVariants({ variant: 'ghost' })}>Documentation</Link>
+                {session ? (
+                    <>
+                        <Link href={"/dashboard"} className={buttonVariants({ variant: 'ghost' })}></Link>
+                        <SignOutButton />
+                    </>
+                ) : <SignInButton />}
+            </div>
+        </div>
+    </div>
 }
 
 export default NavBar
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,14 @@
+import { useTheme } from 'next-themes'
+import { FC } from 'react'
+
+interface ThemeToggleProps {
+  
+}
+
+const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
+  const {setTheme} = useTheme()
+  
+    return <div>ThemeToggle</div>
+}
+
+export default ThemeToggle
